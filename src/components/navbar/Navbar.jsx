e9b8dfe3b91b584455eb67939961728929f49fff
Jelani Days <jonathancdays@gmail.com>
2022-03-24 14:22:03 -0600

    Add navbar styles to navbar.css and component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/logo.svg';
 import'./navbar.css';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] =useState(false);
+
   return (
     <div className="navbar">
       <div className="navbar-links">
@@ -21,6 +23,12 @@ const Navbar = () => {
       <div className="navbar-sign">
         <p>Log In</p>
         <button type="button">Register</button>
+      </div>
+      <div className="navbar-menu">
+        {toggleMenu
+        ? <RiCloseLine color="#fff" size-{27} onClick={() => setToggleMenu(false)}/>
+        : <RiMenu3Line color="#fff" size-{27} onClick={() => setToggleMenu(true)}/>
+      }
       </div>
     </div>
   )
