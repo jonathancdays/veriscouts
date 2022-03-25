7dc259f88381fbe6d895b73de1a0d42a7bb7aa63
Jelani Days <jonathancdays@gmail.com>
2022-03-25 08:26:47 -0600

    Add in navbar and header styles to css and jsx files

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import'./header.css';
+import landingpage from '../../assets/landingpage.JPG';
 
 const Header = () => {
   return (
-    <div>Article</div>
+    <div className="header section__padding" id="home">
+      <div className="header-content">
+        <h1>Simply The Best Reviews.</h1>
+        <div className="header-content__input">
+          <p>Help your community by giving your honest review of those you've worked with.</p>
+          <input type="email" placeholder="Sign up with your email."/>
+          <button type="button">Make a Difference</button>
+        </div>
+        <div className="header-image">
+        {/*<img src={landingpage} alt="landing-page image"/>*/}  
+        </div>
+      </div>
+    </div>
   )
 }
 
