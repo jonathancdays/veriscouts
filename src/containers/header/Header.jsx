68c0fa1903febfd282bf34403b2ce1d6ad807545
Jelani Days <jonathancdays@gmail.com>
2022-03-27 12:11:00 -0600

    Update html files

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import'./header.css';
-import landingpage from '../../assets/landingpage.JPG';
+import React from "react";
+import "../../App.css"
+import "./header.css";
+import landingpage from "../../assets/landingpage.JPG";
 
 const Header = () => {
   return (
     <div className="header section__padding" id="home">
       <div className="header-content">
+        <div className="scale-up-center">
         <h1>Simply The Best Reviews.</h1>
-        <p>Help your community by giving your honest review of those you've worked with.</p>
+        <p>
+          Help your community by giving your honest review of those you've
+          worked with.
+        </p>
         <div className="header-content__input">
-          <textarea type="email" placeholder="Sign up with your email here"/>
+          <textarea type="email" placeholder="Sign up with your email here" />
           <button type="button">Make a Difference</button>
         </div>
-        <div className="header-image">
-        {/*<img src={landingpage} alt="landing-page image"/>*/}  
         </div>
       </div>
-    </div>
-  )
-}
+      <div className="header-image slide-in-fwd-top">
+        <img src={landingpage} alt="landing-page" />
+      </div>
+      </div>
+  );
+};
 
-export default Header
+export default Header;
