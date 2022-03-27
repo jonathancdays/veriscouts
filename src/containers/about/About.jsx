07df49908c7efdde3b81c44ca632e9cd7382b0eb
Jelani Days <jonathancdays@gmail.com>
2022-03-27 13:29:22 -0600

    Add styles to index, brand, about, and header css and jsx

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import'./about.css';
+import React from "react";
+import Feature from "../../components/feature/Feature";
+import "./about.css";
 
 const What = () => {
   return (
-    <div>Article</div>
-  )
-}
+    <div className="about section__margin" id="about">
+      <div className="about-feature">
+        <Feature />
+      </div>
+      <div className="about-heading">
+        <h1 className="about__text">
+          Connect with your community and make a difference.
+        </h1>
+        <p>Explore Reviews</p>
+      </div>
+      <div className="about-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
+};
 
-export default What
+export default What;
