878307cbc91446d7115319acbd03d72dc74dfc06
Jelani Days <jonathancdays@gmail.com>
2022-03-28 15:48:40 -0600

    Replace blog photos. Update article jsx and css. Add blog and footer css and jsx.

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import'./footer.css';
+import React from "react";
+import "./footer.css";
+import logo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <div>Article</div>
-  )
-}
+    <div className="footer section__padding">
+      <div className="footer-heading">
+        <h1>
+          Help your community by giving your honest review of those you've
+          worked with.
+        </h1>
+      </div>
+      <div className="footer-btn">
+        <p>Make A Difference</p>
+      </div>
+      <div className="footer-links">
+        <div className="footer-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="footer-links_div">
+          <h4>Links</h4>
+          <p>Account</p>
+          <p>Social Media</p>
+          <p>Reviews</p>
+          <p>Jobs</p>
+        </div>
+        <div className="footer-links_div">
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Accessibility Statement</p>
+        </div>
+        <div className="footer-links_div">
+          <h4>Contact</h4>
+          <p>Address</p>
+          <p>Phone</p>
+          <p>E-Mail</p>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <p>
+          Copyright Disclaimer under Section 107 of the copyright act 1976,
+          allowance is made for fair use for purposes such as criticism,
+          comment, news reporting, scholarship, and research.
+        </p>
+        <p>
+        Disclaimer: Under the Fair Use Copyright Act I have taken
+            inspiration and my experience from my previous employer to design
+            and code a personal project for creative purposes. I do not own any
+            Veriscouts assets.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
